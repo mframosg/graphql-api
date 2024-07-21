1acd7aa260145d265ea66e5558f09dff23a61826
Miguel Ramos <mrmiguel0105@gmail.com>
2024-07-21 01:18:03 -0500

    fill table modified

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ class User(models.Model):
     idusuario = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
     class Meta:
         db_table = 'usuarios'
         
--- users/views.py
+++ users/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from faker import Faker
 from .models import User
+from django.core.exceptions import ValidationError
+
 
 fake = Faker()
 
 def create_random_users(num_entries):
-    User.objects.all().delete()  # Truncate the table
+    if not isinstance(num_entries, int) or num_entries <= 0:
+        raise ValueError("num_entries debe ser un número entero positivo")
 
-    users = [User(name=fake.name(), age=fake.random_int(min=18, max=70)) for _ in range(int(num_entries))]
+    User.objects.all().delete()
+
+    users = []
+    for _ in range(num_entries):
+        gender = fake.random_element(elements=['M', 'F'])
+        name = fake.name_male() if gender == 'M' else fake.name_female()
+        age = fake.random_int(min=18, max=70)
+        user = User(name=name, age=age, gender=gender)
+        
+        try:
+            user.full_clean()
+            users.append(user)
+        except ValidationError as e:
+            print(f"Error validating user: {e.message_dict}")
 
     User.objects.bulk_create(users)
